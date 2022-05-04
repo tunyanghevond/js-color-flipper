9b1da2e1e15a46061be30d80d7285b6a115f0489
Ghevond Tunyan <ghtunyan2018@gmail.com>
2022-05-04 17:47:23 +0200

    add functionality and finish the project

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.querySelector('.btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function () {
+    const randomNum = colorGenerator();
+    console.log(randomNum)
+    document.body.style.backgroundColor = colors[randomNum];
+    color.textContent = colors[randomNum]
+})
+
+function colorGenerator() {
+    return Math.floor(Math.random() * colors.length);
+}
